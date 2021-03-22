9bbcc7c55bf69868495831f7d1221eca15ec4e15
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-03-22 11:50:34 +0100

    background image and overlay responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import "./index.scss";
+
 export default function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <section className="catchThatEye">
+        <img className="catchThatEye__backgroundImage" src="./Background.png" alt="Background"/>
+        <div className="catchThatEye__backgroundImageOverlayOne"></div>
+        <div className="catchThatEye__backgroundImageOverlayTwo"></div>
+      </section>
     </div>
   );
 }
