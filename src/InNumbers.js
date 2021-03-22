3f38c12425b6285a10086c1fe267ecfb334cbe92
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-03-22 14:21:00 +0100

    WHOAH MOMMA

--- src/InNumbers.js
+++ src/InNumbers.js
@@ -1,16 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Heading from "./components/Heading";
 import NumberBox from "./components/NumberBox";
 import "./InNumbers.scss";
 
 export default function InNumbers(){
+    var [content, setContent] = useState([]);
+
+    useEffect(
+        function(){
+            axios
+                .get("./Numbers.json")
+                .then(function(response){
+                    setContent(response.data);
+                });
+        }, [setContent]);
+
     return(
         <article className="inNumbers">
             <Heading highlight="Our conference in numbers" />
-            <div>
-                <NumberBox text="Countries" number="12" />
-                <NumberBox text="Cities" number="30" />
-                <NumberBox text="Speakers" number="120" />
-                <NumberBox text="Attendees" number="750+" />
+
+            <div className="inNumbers__boxes">
+                {content.items?.map(function(item){
+                    return(
+                        <NumberBox text={item.text} number={item.number} />
+                    )
+                })}
             </div>
         </article>
     )
